70663ed858b0200bf08311d90c448094860ac971
Sanae <susenmori+git@gmail.com>
2024-08-02 14:15:16 +0200

    Environment variables for paths and file organizer

---
+++ Shift_Explanation/path_organizer.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+
+# Pfad zur .env-Datei
+dotenv_path = 'environment.env'  # Wenn die .env-Datei im selben Verzeichnis wie das Skript liegt
+
+# Laden der Umgebungsvariablen aus der .env-Datei
+load_dotenv(dotenv_path)
+
+# Zugriff auf die Umgebungsvariablen
+path_root = os.getenv('OUTPUT_PATH_FOR_ANALYSIS')
+input ("Did you change the folder name?")
+folder_name = os.getenv('CURRENT_FOLDER_NAME')  # TODO: Please change this variable accordingly
+path_customer_profiles = os.getenv('OUTPUT_CUSTOMER_PROFILES_PATHS')
+path_distances = os.getenv('OUTPUT_DISTANCES')
+path_shift_explanations = os.getenv('OUTPUT_SHIFT_EXPLANATIONS')
+path_transport_graphics = os.getenv('OUTPUT_TRANSPORT_GRAPHICS')
+
+# Create list of to-be-created folder paths
+paths = [path_customer_profiles, path_distances, path_shift_explanations, path_transport_graphics]
+
+# Beispiel für die Nutzung der Umgebungsvariablen
+print(f'OUTPUT_PATH_FOR_ANALYSIS: {path_root}')
+
+# Erstellen des Hauptordners
+new_folder_path = os.path.join(path_root+folder_name)
+print("Output Hauptordner:", new_folder_path)
+
+# Sicherstellen, dass der Hauptordner existiert
+os.makedirs(new_folder_path, exist_ok=True)
+
+full_paths = []
+# Erstellen der Unterordner
+for path in paths:
+    full_path = os.path.join(new_folder_path+path)
+    print("Erstellen des Unterordners:", full_path)
+    os.makedirs(full_path, exist_ok=True)
+    full_paths.append(full_path)
+
+path_customer_profiles = full_paths[0]
+path_distances = full_paths[1]
+path_shift_explanations = full_paths[2]
+path_transport_graphics = full_paths[3]
+
+print("#"*50)
+print(path_customer_profiles)
