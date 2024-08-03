c82da0a68027101007f50b2453c50c178bd5a30d
Eric Duhme <eric.duhme@t-online.de>
2024-08-03 14:35:30 +0200

    Minor improvements

--- Shift_Explanation/path_organizer.py
+++ Shift_Explanation/path_organizer.py
@@ -18,36 +18,39 @@ path_transport_graphics = os.getenv('OUTPUT_TRANSPORT_GRAPHICS')
 # Create list of to-be-created folder paths
 paths = [path_customer_profiles, path_distances, path_shift_explanations, path_transport_graphics]
 
-# Beispiel für die Nutzung der Umgebungsvariablen
-print(f'OUTPUT_PATH_FOR_ANALYSIS: {path_root}')
 
 # Erstellen des Hauptordners
 new_folder_path = os.path.join(path_root+folder_name)
 print("Output Hauptordner:", new_folder_path)
 
 # Sicherstellen, dass der Hauptordner existiert
-os.makedirs(new_folder_path, exist_ok=False)
+try:
+    os.makedirs(new_folder_path, exist_ok=False)
+except: # Falls der Ordner bereits existiert
+    print("Der Ordner existiert bereits")
 
 full_paths = []
 # Erstellen der Unterordner
 for path in paths:
     full_path = os.path.join(new_folder_path+path)
-    print("Erstellen des Unterordners:", full_path)
-    os.makedirs(full_path, exist_ok=False)
+    try:
+        os.makedirs(full_path, exist_ok=False)
+    except: # Falls der Ordner bereits existiert
+        print("Der Ordner existiert bereits")
     full_paths.append(full_path)
 
+# Speichern der Pfade in Textdateien
+
 path_customer_profiles = full_paths[0]
+path_distances = full_paths[1]
+path_shift_explanations = full_paths[2]
+path_transport_graphics = full_paths[3]
+
 with open(path_root + "/File_Paths/path_customer_profiles.txt", "w") as f:
     f.write(path_customer_profiles)
-path_distances = full_paths[1]
 with open(path_root + "/File_Paths/path_distances.txt", "w") as f:
     f.write(path_distances)
-path_shift_explanations = full_paths[2]
 with open(path_root + "/File_Paths/path_shift_explanations.txt", "w") as f:
     f.write(path_shift_explanations)
-path_transport_graphics = full_paths[3]
 with open(path_root + "/File_Paths/path_transport_graphics.txt", "w") as f:
     f.write(path_transport_graphics)
-
-print("#"*50)
-print(path_customer_profiles)
