adb0164ec0fbe5f1ccb602c3e6de61f03f9f7f73
Eric Duhme <eric.duhme@t-online.de>
2024-08-03 16:06:04 +0200

    Small improvements

--- Shift_Explanation/run_experiment.py
+++ Shift_Explanation/run_experiment.py
@@ -1,32 +1,30 @@
 import papermill as pm
 import os
-import sys
 
+# Setting up the correct working directory
 
-import os
-print(os.getcwd())
 os.chdir('Shift_Explanation')
 print(os.getcwd())
 
-def execute_notebook_with_params(notebook_path, parameters, temp_output_path):
+def execute_notebook_with_params(notebook_path, parameters, output_path):
     pm.execute_notebook(
         notebook_path,
-        temp_output_path,
+        output_path,
         parameters=parameters
     )
 
 
-# Parameter-Sätze definieren
+# define the parameters for the notebook
 parameter_sets = [
     {'comparison_id': 0,},
     {'comparison_id': 1},
     {'comparison_id': 2}
 ]
 
-# Notebook-Pfad
+# Notebook path
 notebook_path = 'adult-income-experiment.ipynb'
-temp_output_path = notebook_path
+output_path = notebook_path
 
-# Notebook mit verschiedenen Parametern ausführen
+# Running the notebook with the 3 dataset pairs
 for params in parameter_sets:
-    execute_notebook_with_params(notebook_path, params, temp_output_path)
+    execute_notebook_with_params(notebook_path, params, output_path)
