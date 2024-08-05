90be728e7bfeb69d9dfc3863f4a5ffb781826818
Eric Duhme <eric.duhme@t-online.de>
2024-08-05 16:22:15 +0200

    More deletions

--- notebooks/nba_analysis/Test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-from scipy.stats import ks_2samp
-
-# Load the dataset
-df = pd.read_excel('/Users/sanaemessoudi/Desktop/Projekte/PAMI_Projekt9/notebooks/nba_analysis/real_datasets/NBA_Data.xlsx', sheet_name=1)
-print(df)
