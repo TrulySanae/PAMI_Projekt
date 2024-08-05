1b361997eda77cf20726bfc914bd82db0abd6545
Eric Duhme <eric.duhme@t-online.de>
2024-08-05 16:10:08 +0200

    Create a dataframe for each regression measurement result

--- Shift_Explanation/analyse_results.py
+++ Shift_Explanation/analyse_results.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import os
 
 def create_summary():
+  print('analyse_results.py Directory')
+  print(os.getcwd())
   # Load the CSV files
-  path_distances = open('./Results_Experiments/File_Paths/path_distances.txt','r').read()
+  path_distances = open('Shift_Explanation/Results_Experiments/File_Paths/path_distances.txt','r').read()
   for i in range(0,5,2):
-      path = f'../{path_distances}/results_customer_profile_{i}_and_customer_profile_{i+1}.csv'
+      path = f'{path_distances}/results_customer_profile_{i}_and_customer_profile_{i+1}.csv'
       globals()[f'df_{i}'] = pd.read_csv(path)
 
   # Concatenate the dataframes
@@ -20,8 +23,8 @@ def create_summary():
     "Standard_Deviation": [std_con,std_wei,std_uni]
   })
   deviations.sort_values(by='Standard_Deviation')
-  path_shift_explanations = open('./Results_Experiments/File_Paths/path_shift_explanations.txt','r').read()
-  output_path = f"../{path_shift_explanations}/Calculated_Variances.csv"
+  path_shift_explanations = open('Shift_Explanation//Results_Experiments/File_Paths/path_shift_explanations.txt','r').read()
+  output_path = f"{path_shift_explanations}/Calculated_Variances.csv"
   deviations.to_csv(output_path, index=False)
 
 
@@ -31,8 +34,31 @@ def create_summary():
   X = sm.add_constant(X)
   model = sm.OLS(y, X).fit()
   model_summary = model.summary()
+  
+    # Extract relevant values from the regression summary
+  regression_results = {
+        "Measurement": ["R-squared", "Adj. R-squared",
+                        "const_coef", "const_std_err", "const_p>|t|",
+                        "Connections_diff_coef", "Connections_diff_std_err", "Connections_diff_p>|t|",
+                        "Weight_diff_coef", "Weight_diff_std_err", "Weight_diff_p>|t|",
+                        "Unit_diff_coef", "Unit_diff_std_err", "Unit_diff_p>|t|"],
+        "Values": [model.rsquared, model.rsquared_adj,
+                    model.params['const'], model.bse['const'], model.pvalues['const'],
+                    model.params['Connections_diff'], model.bse['Connections_diff'], model.pvalues['Connections_diff'],
+                    model.params['Weight_diff'], model.bse['Weight_diff'], model.pvalues['Weight_diff'],
+                    model.params['Unit_diff'], model.bse['Unit_diff'], model.pvalues['Unit_diff']]
+    }
+
+    # Convert the dictionary to a dataframe
+  regression_results_df = pd.DataFrame(regression_results)
+
+    # Save the dataframe to a CSV file
+  regression_results_path = f"{path_shift_explanations}/regression_results.csv"
+  regression_results_df.to_csv(regression_results_path, index=False)
+
+  print(model_summary)
+  
   # Save the regression results to a text file
-  with open(f'../{path_shift_explanations}/regression_summary.txt', 'w') as f:
+  with open(f'{path_shift_explanations}/regression_summary.txt', 'w') as f:
       f.write(model_summary.as_text())
       
-  print(model_summary)
