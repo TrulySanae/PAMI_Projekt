57a3cc6d11f6af5a2a663107a42e7e9546875470
Eric Duhme <eric.duhme@t-online.de>
2024-08-06 16:20:20 +0200

    Updated to match the outsourced data splitting

--- NBA_Data_Analysis/data_utils.py
+++ NBA_Data_Analysis/data_utils.py
@@ -2,34 +2,25 @@ import pandas as pd
 from sklearn.utils import check_random_state
 
 # Function to load and preprocess logistics data
-def load_and_preprocess_logistics_data(random_state=None, max_samples=None, return_column_names=False):
+def load_and_preprocess_nba_data(random_state=None, max_samples=None, return_column_names=False):
     rng = check_random_state(random_state)
 
     # Define file paths for dataset
-    file_path = '/Users/sanaemessoudi/Desktop/Projekte/PAMI_Projekt9/NBA_Data_Analysis/NBA_Data.xlsx'
+    file_path_a = './Source_Target/source_data.csv'
+    file_path_b = './Source_Target/target_data.csv'
+    file_path_c = './Source_Target/experiment_parameters.csv'
 
-    # Load dataset
-    data = pd.read_excel(file_path, sheet_name=1)
+    source_data = pd.read_csv(file_path_a)
+    target_data = pd.read_csv(file_path_b)
+    parameters = pd.read_csv(file_path_c)
 
-    # Correctly filter data based on TEAM and Year
-    # source_data = data[(data["Team"] == "LAL") & (data["Year"] == 2023)].copy()
-    # target_data = data[(data["Team"] == "CLE") & (data["Year"] == 2023)].copy()
-
-    source_data = data[(data["Team"] == "LAL")].copy()
-    target_data = data[(data["Team"] == "CLE")].copy()
-
-    print("######################## Source_Data_A ##########################")
-    print(source_data)
-    print("########################### Source_Data_B #######################")
-    print(target_data)
-    print("##################################################")
 
     # Drop columns that are not needed
-    columns_to_drop = ['MinutesPlayed', 'PointsScored', 'FieldGoalsMade', 'FieldGoalsAttempted',
-                       'ThreePointFieldGoalsMade', 'ThreePointFieldGoalsAttempted', 'FreeThrowsMade',
-                       'FreeThrowsAttempted', 'OffensiveRebounds', 'DefensiveRebounds', 'FantasyPoints',
-                       'DoubleDoubles', 'TripleDoubles', 'Player', 'Team', 'Year']
-
+    # columns_to_drop = ['MinutesPlayed', 'PointsScored', 'FieldGoalsMade', 'FieldGoalsAttempted',
+    #                    'ThreePointFieldGoalsMade', 'ThreePointFieldGoalsAttempted', 'FreeThrowsMade',
+    #                    'FreeThrowsAttempted', 'OffensiveRebounds', 'DefensiveRebounds', 'FantasyPoints',
+    #                    'DoubleDoubles', 'TripleDoubles', 'Player', 'Team', 'Year']
+    columns_to_drop = ['PLAYER', 'TEAM', 'Year']
     for df in [source_data, target_data]:
         df.drop(columns=columns_to_drop, inplace=True)
 
@@ -43,8 +34,7 @@ def load_and_preprocess_logistics_data(random_state=None, max_samples=None, retu
     source = source_data.to_numpy().astype(float)
     target = target_data.to_numpy().astype(float)
 
-    print(f'Finished preprocessing logistic dataset. ',
-          f'Split on Data A and Data B with resulting source shape: {source.shape}, target shape: {target.shape}.')
+    print(f'Splitting on {parameters['split_feature'].values[0]} with {parameters['instance_1'].values[0]} and {parameters['instance_2'].values[0]} resulting source shape: {source.shape}, target shape: {target.shape}.')
 
     print("###############################")
     print(source_data.columns.to_list())
@@ -61,5 +51,4 @@ def load_and_preprocess_logistics_data(random_state=None, max_samples=None, retu
     else:
         return source, target
 
-# Call the function (example)
-source, target = load_and_preprocess_logistics_data()
+
